4e4d81efd94ef0ff2e00fa75f730efba7444c7d0
Dylane <tanolamene@gmail.com>
2025-01-04 16:42:05 -0400

    First commit

---
+++ theorie/src/App.jsx
@@ -0,0 +1,21 @@
+import './assets/scss/style.scss'
+
+
+function App() {
+
+
+  return (
+    <>
+      <h1 className='text-3xl underline text-blue-500 text-center'>hello world</h1>
+
+      <p>hello world</p>
+      <ul>
+        <li><a href=""></a></li>
+        <li><a href=""></a></li>
+        <li><a href=""></a></li>
+      </ul>
+    </>
+  )
+}
+
+export default App
