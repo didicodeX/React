5b3e8c395ae5c8e93422bdbf1b32ec4c61fb8bdb
Dylane <tanolamene@gmail.com>
2025-01-05 10:20:16 -0400

    Refactor styles and remove unused components

--- theorie/src/components/greeting.jsx
+++
@@ -1,4 +0,0 @@
-export function Greeting({ name, age = 16, majeur=true }) {
-  console.log(name, age, majeur);
-  return <h1>hello { name }!</h1>;
-}
--- theorie/src/pages/Home.jsx
+++ theorie/src/pages/Home.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 
 const Home = () => {
+  const handleClick = () => {
+    console.log("click")
+  }
+
   return (
     <div>
       <h1>Accueil</h1>
+      <button onClick={handleClick}>Submit</button>
     </div>
   );
 };
