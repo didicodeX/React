8585e586f2338864c94d07d62bbc0506f273f13a
Dylane <tanolamene@gmail.com>
2025-01-08 10:12:13 -0400

    Enhance form component with validation and dynamic field management; add error handling styles

--- theorie/src/pages/Form.jsx
+++ theorie/src/pages/Form.jsx
@@ -1,20 +1,102 @@
+import { zodResolver } from "@hookform/resolvers/zod";
 import React from "react";
-import { useForm } from "react-hook-form";
+import { useFieldArray, useForm } from "react-hook-form";
 
 //npm i react-hook-form yup @hookform/resolvers
 
 const Form = () => {
-  const {register, handleSubmit, getValues} = useForm();
+  const zodSchema = {
+    valueAsNumber: true,
+  };
+
+  const defaultValues = {
+    name: "Pop John",
+    activities: [],
+  };
+
+  const {
+    control, //permet d'ajouter des champs
+    register,
+    handleSubmit,
+    getValues,
+    watch,
+    reset,
+    setError,
+    formState: { errors, isSubmitting },
+  } = useForm({
+    defaultValues,
+    // resolver: zodResolver(zodSchema),
+    mode: "onSubmit", // permet de preciser a quel moment je fait de la validation
+  });
+
+  const { fields, append, remove } = useFieldArray({
+    name: "activities",
+    control,
+  });
+
+  function addActivities() {
+    append({
+      value: "uoo",
+    });
+  }
+
+  function deleteActivity(index) {
+    remove(index);
+  }
 
   function submit(values) {
     console.log(values);
   }
-  console.log(getValues());
-  
+  // console.log(errors);
+
   return (
     <form className="form" onSubmit={handleSubmit(submit)}>
-      <input {...register("name")} type="text" placeholder="name" id="name" />
+      <input
+        className={errors.name ? "input-error" : ""}
+        {...register("name", {
+          maxLength: {
+            value: 3,
+            message: "Trop long !",
+          },
+        })}
+        type="text"
+        placeholder="name"
+        id="name"
+      />
+      <input
+        {...register("age", { valueAsNumber: true })}
+        type="number"
+        name="age"
+        id="age"
+      />
+      <div>
+        <span>Activities </span>
+        <button onClick={addActivities} type="button" className="button">
+          +
+        </button>
+      </div>
+      <ul>
+        {fields.map((activity, i) => (
+          <li key={activity.id}>
+            <input
+              {...register(`activities[${i}].value`)}
+              type="text"
+              name="activity"
+              placeholder="activity"
+            />
+            <button
+              onClick={() => deleteActivity(i)}
+              type="button"
+              className="button"
+            >
+              {" "}
+              -{" "}
+            </button>
+          </li>
+        ))}
+      </ul>
       <input type="submit" value="Save" onSubmit={handleSubmit} />
+      {errors?.name && <p className="errors">{errors.name.message} </p>}
     </form>
   );
 };
